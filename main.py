f1505832c22f84bef2e2803aa71f1a1a67857fa6
Hussein Jundi <husseinjundi810@gmail.com>
2019-03-06 13:16:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from arsenl import Arsel
 
 if __name__ = "__main__":
 
-    adj ='arsel/arsenl_a.txt'
-    verb ='arsel/arsenl_v.txt'
-    adv = 'arsel/arsenl_r.txt'
-    nouns = 'arsel/arsenl_n.txt'
+    adj ='arsel/arsel_a.txt'
+    verb ='arsel/arsel_v.txt'
+    adv = 'arsel/arsel_r.txt'
+    nouns = 'arsel/arsel_n.txt'
     arsenl = Arsenl(adj,nouns,verb,adv)
